ed40120136a0ff1b374e7ef29e3efb710abb15d5
rajanlamic <rajanlamic@gmail.com>
2016-05-18 11:09:03 +0100

    each screenshots and digitalData pretify

--- e2e/asm1/tests/loginTest.js
+++ e2e/asm1/tests/loginTest.js
@@ -11,6 +11,7 @@ module.exports = {
     load: function(browser) {
         this.page = browser.page.login();
         this.page.navigate();
+        browser.saveScreenshot('e2e/asm1/screenshots/login/loadAsm1.png');
     },
     preLogin: function(browser) {
         browser.pause(1000);
@@ -18,6 +19,8 @@ module.exports = {
     },
     login: function(browser) {
         browser.pause(1000);
+        browser.saveScreenshot('e2e/asm1/screenshots/login/login.png');
+
         this.page.setValue('@userNameTxt', 'ccuser@test.com');
         this.page.setValue('@passwordTxt', 'Password1');
         this.page.click('@loginBtn');
@@ -28,13 +31,14 @@ module.exports = {
             //.assert.title("Sainsbury's online Grocery Shopping and Fresh Food Delivery")
             .assert.visible('@homepage')
             .assert.containsText('@homepage', 'Hello');
+        browser.saveScreenshot('e2e/asm1/screenshots/login/homepage.png');
     },
     saveDigitalData : function(browser) {
         var self = this;
         browser
             .execute("return window.digitalData;", [], function(response) {
                 self.digitalData = response.value;
-                fs.writeFile("e2e/asm1/digitalData/homepage/digitalData.txt", JSON.stringify(response.value) , 'utf8', function(err) {
+                fs.writeFile("e2e/asm1/files/homepage.json", JSON.stringify(response.value, null, 2) , 'utf8', function(err) {
                     if(err) {
                         return console.log(err);
                     }
